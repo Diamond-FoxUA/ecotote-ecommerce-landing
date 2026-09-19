f76ec4eb8db94c6a99e1d3852cc7ef4f98d78894
Dmytro Farbun <dimofarb@gmail.com>
2026-09-19 21:50:27 +0300

    feat: add social message to dictionaries and enhance accessibility in SocialLinks component

--- src/shared/ui/SocialLinks.tsx
+++ src/shared/ui/SocialLinks.tsx
@@ -1,11 +1,17 @@
+"use client";
+
 import Icon from "@/shared/ui/Icon";
 import Link from "next/link";
+
+import { useDictionary } from "../context/DictionaryContext";
 
 type SocialLinksProps = {
   className?: string;
 };
 
 export default function SocialLinks({ className }: SocialLinksProps) {
+  const dict = useDictionary();
+
   const socialLinks = [
     {
       id: 1,
@@ -31,9 +37,9 @@ export default function SocialLinks({ className }: SocialLinksProps) {
           key={l.id}
           className="hover:scale-120 active:scale-90 transition-transform duration-300"
         >
-          <Link href={l.link}>
+          <Link href={l.link} aria-label={`${dict.common.socialMsg} ${l.text}`}>
             <Icon
-              aria-label={l.text}
+              aria-hidden="true"
               iconName={`icon-${l.text}`}
               size={32}
               className={className ? className : ""}
