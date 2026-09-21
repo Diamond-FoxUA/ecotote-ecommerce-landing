feb36d2e476fc80365025fc7c2526f8fc3401453
Dmytro Farbun <dimofarb@gmail.com>
2026-09-21 12:45:27 +0300

    feat: add imageSizes attribute for enhanced image responsiveness

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     qualities: [75, 90],
+    imageSizes: [288, 310, 340, 576, 620, 680]
   },
 
   async redirects() {
